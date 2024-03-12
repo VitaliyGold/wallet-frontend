cb5e6c5e34287d8f4ba2d83c837d37b0df25bfee
Vitalii Prokhorov <v.prokhorov@kryptonite.ru>
2024-03-12 23:43:42 +0300

    fix filter

--- src/widgets/expensesFilters/lib/isEqualFilter.ts
+++ src/widgets/expensesFilters/lib/isEqualFilter.ts
@@ -1,7 +1,7 @@
 import type { ExpensesFilters } from "@/entities/expenses";
 
 const isEqualFilter = (oldValue: ExpensesFilters, newValue: ExpensesFilters): boolean => {
-    return JSON.stringify(oldValue) === JSON.stringify(newValue);
+    return JSON.stringify(oldValue).toLocaleLowerCase() === JSON.stringify(newValue).toLocaleLowerCase();
 }
 
 export {
