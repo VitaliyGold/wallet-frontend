6558f60ca9876549adac45a66d507b4b2b93d335
Vitalii Prokhorov <v.prokhorov@kryptonite.ru>
2024-03-17 13:08:35 +0300

    add list of category

--- src/widgets/categoryCloud/CategoryCloud.tsx
+++ src/widgets/categoryCloud/CategoryCloud.tsx
@@ -1,11 +1,19 @@
-import { UiText, WidgetContainer } from "@/shared/ui";
+import { useSelector } from "react-redux";
 
+import { CategoryLabel, categoryListSelector } from "@/entities/category";
+import { WidgetContainer } from "@/shared/ui";
 
+import styles from './styles.module.less';
 
 const CategoryCloud = () => {
+
+    const categoryList = useSelector(categoryListSelector.selectAll);
+
     return (
         <WidgetContainer widgetTitle="Категории" widgetHeight="auto">
-
+            <div className={styles.categoryCloud}>
+                { categoryList.map(category => <CategoryLabel size="large">{category.name}</CategoryLabel>) }
+            </div>
         </WidgetContainer>
     )
 };
