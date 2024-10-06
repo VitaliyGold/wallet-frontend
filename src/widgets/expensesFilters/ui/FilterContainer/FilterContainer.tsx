1ae8e40c3cbbb038902b5e86305aff5dc97444af
kysko68 <kysko68@yandex.ru>
2024-10-07 01:04:58 +0300

    replace filters on expenses list page

---
+++ src/widgets/expensesFilters/ui/FilterContainer/FilterContainer.tsx
@@ -0,0 +1,20 @@
+import type { FC, ReactNode } from 'react';
+
+import styles from './styles.module.less';
+
+interface Props {
+    children: ReactNode;
+    maxWidth: number;
+}
+
+const FilterContainer: FC<Props> = ({ children, maxWidth }) => {
+    return (
+        <div className={styles.container} style={{ maxWidth: `${maxWidth}px` }} >
+            {children}
+        </div>
+    )
+};
+
+export {
+    FilterContainer,
+}
